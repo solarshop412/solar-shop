03ca04ccafcd5c7b989e5359661adc423e0429d9
Ivan Goricki <ivan.goricki@solarni-paneli.hr>
2026-02-19 15:40:26 +0100

    b2b routes organised

--- src/app/features/b2b/b2b.routes.ts
+++ src/app/features/b2b/b2b.routes.ts
@@ -5,7 +5,7 @@ import { CompanyApprovedGuard } from '../../guards/company-approved.guard';
 
 export const B2B_ROUTES: Routes = [
   {
-    path: '',
+    path: 'partneri',
     component: B2bLayoutComponent,
     children: [
       {
@@ -13,6 +13,20 @@ export const B2B_ROUTES: Routes = [
         loadComponent: () =>
           import('./partners/partners.component').then(
             (m) => m.PartnersComponent,
+          ),
+      },
+      {
+        path: 'o-nama',
+        loadComponent: () =>
+          import('./partners/about/partners-about.component').then(
+            (m) => m.PartnersAboutComponent,
+          ),
+      },
+      {
+        path: 'registracija',
+        loadComponent: () =>
+          import('./partners/register/partners-register.component').then(
+            (m) => m.PartnersRegisterComponent,
           ),
       },
       {
@@ -24,12 +38,85 @@ export const B2B_ROUTES: Routes = [
         canActivate: [CompanyApprovedGuard],
       },
       {
+        path: 'proizvodi/:id',
+        loadComponent: () =>
+          import('./partners/products/partners-product-details/partners-product-details.component').then(
+            (m) => m.PartnersProductDetailsComponent,
+          ),
+        canActivate: [CompanyApprovedGuard],
+      },
+      {
+        path: 'ponude',
+        loadComponent: () =>
+          import('./partners/offers/partners-offers/partners-offers.component').then(
+            (m) => m.PartnersOffersComponent,
+          ),
+        canActivate: [CompanyApprovedGuard],
+      },
+      {
+        path: 'ponude/:id',
+        loadComponent: () =>
+          import('./partners/offers/partners-offer-details/partners-offer-details.component').then(
+            (m) => m.PartnersOfferDetailsComponent,
+          ),
+        canActivate: [CompanyApprovedGuard],
+      },
+      {
         path: 'profil',
         loadComponent: () =>
           import('./partners/profile/partner-profile.component').then(
             (m) => m.PartnerProfileComponent,
           ),
         canActivate: [AuthGuard],
+      },
+      {
+        path: 'detalji-narudzbe/:id',
+        loadComponent: () =>
+          import('./order-details/b2b-order-details.component').then(
+            (m) => m.B2bOrderDetailsComponent,
+          ),
+        canActivate: [AuthGuard],
+      },
+      {
+        path: 'kontakt',
+        loadComponent: () =>
+          import('./partners/contact/partners-contact.component').then(
+            (m) => m.PartnersContactComponent,
+          ),
+      },
+
+      // B2B Checkout routes - Lazy loaded
+      {
+        path: 'blagajna',
+        loadComponent: () =>
+          import('./checkout/b2b-checkout.component').then(
+            (m) => m.B2bCheckoutComponent,
+          ),
+        canActivate: [CompanyApprovedGuard],
+        children: [
+          { path: '', redirectTo: 'pregled-narudzbe', pathMatch: 'full' },
+          {
+            path: 'pregled-narudzbe',
+            loadComponent: () =>
+              import('./checkout/steps/b2b-order-review/b2b-order-review.component').then(
+                (m) => m.B2bOrderReviewComponent,
+              ),
+          },
+          {
+            path: 'dostava',
+            loadComponent: () =>
+              import('./checkout/steps/b2b-shipping/b2b-shipping.component').then(
+                (m) => m.B2bShippingComponent,
+              ),
+          },
+          {
+            path: 'placanje',
+            loadComponent: () =>
+              import('./checkout/steps/b2b-payment/b2b-payment.component').then(
+                (m) => m.B2bPaymentComponent,
+              ),
+          },
+        ],
       },
     ],
   },
